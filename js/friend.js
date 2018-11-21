7246e81c8c8fa191a1aaebeaf917c1840e19529b
Crystal R Grave-Montenegro <cgravemontenegr1@nait.ca>
2018-11-21 08:52:05 -0700

    collecting friend data and display, cut down on code by using javascript array. Almost like calling constructor function. friend(friendData)

--- js/friend.js
+++ js/friend.js
@@ -8,11 +8,12 @@ const friendStyle = {
 
 //  Passing simple props to the component
 const Friend = (props) => {
+    const {friendData} = props;
     return (
         <div style={friendStyle}>
-            <img src={props.avatar}/>
-            <div>{props.firstName} {props.lastName}</div>
-            <div>{props.email}</div>
+            <img src={friendData.avatar}/>
+            <div>{friendData.firstName} {friendData.lastName}</div>
+            <div>{friendData.email}</div>
         </div>
     );
 }
--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@ const domContainer = document.querySelector('.react-container');
 // const myComponent = React.createElement(Greeting);
 // ReactDOM.render(<Greeting name="Jane Doe"/>, domContainer);
 
+const friendData = [
+    {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        avatar: 'jdog.jpg',
+        email:'jdoe@example.com'
+    },
+    {
+        firstName: 'Sally',
+        lastName: 'Anne',
+        avatar: 'salann.jpg',
+        email:'sanne@example.com'
+    }
+]
+
 const App = () => {
     return (
         <div>
